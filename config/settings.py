27b779606ab68254db1c42090ca921afb276c1b8
Federico Sosa <lfedericososa@hotmail.com>
2026-05-02 20:40:09 -0300

    config/settings correccion

--- config/settings.py
+++ config/settings.py
@@ -28,11 +28,18 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG') == 'True'
+DEBUG = False
 
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+     "127.0.0.1",
+    "localhost",
+    "chorifans.pythonanywhere.com",
+]
 
+CSRF_TRUSTED_ORIGINS = [
+    "https://chorifans.pythonanywhere.com",
+]
 
 # Application definition
 
