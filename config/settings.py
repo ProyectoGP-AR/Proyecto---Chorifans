991ee0e40143aa5e314879cfe5a49fc4bd755978
Federico Sosa <lfedericososa@hotmail.com>
2026-05-02 21:02:53 -0300

    config/settings correccion

--- config/settings.py
+++ config/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 
 ALLOWED_HOSTS = [
@@ -149,7 +149,7 @@ STATICFILES_DIRS = [
 ]
 
 # Opcionalmente, para cuando se haga deploy (no afecta en desarrollo)
-# STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 
 # Archivos subidos por el usuario (imágenes, etc.)
